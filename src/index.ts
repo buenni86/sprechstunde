a5b26c2b15f4ae1680ba497f72ec65e17197e271
buenni86 <12584248+buenni86@users.noreply.github.com>
2022-06-10 14:09:27 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
 import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+import { ButtonDescriptor } from "@workadventure/iframe-api-typings/Api/iframe/Ui/ButtonDescriptor";
 
 console.log('Script started successfully');
 	
